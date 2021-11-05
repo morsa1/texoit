37d0c817036c397f4d296c9c23ce3337d9a5e47c
Edilson <edilson_juni@hotmail.com>
2021-11-05 01:34:29 -0300

    TESTE E2E almost done

---
+++ cypress/support/elements/Dresses.js
@@ -0,0 +1,7 @@
+class Dresses {
+    printeddress = () => { return 'product-name' }
+  
+    botaoRecuperarSenha = () => { return '.login__form__forgot-password' }
+  }
+  
+  export default LoginElements;
